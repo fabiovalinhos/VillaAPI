0a397db4a349efdcd44a17fdaaac1dc834536637
fabiovalinhos <fabiovalinhos@gmail.com>
2023-11-12 14:09:57 -0300

    Pondo no CreateAtRoutes no CreateVilla

--- src/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ src/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -18,7 +18,7 @@ namespace MagicVilla_VillaAPI.Controllers
             return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -42,7 +42,7 @@ namespace MagicVilla_VillaAPI.Controllers
         }
 
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
@@ -60,7 +60,7 @@ namespace MagicVilla_VillaAPI.Controllers
 
             VillaStore.villaList.Add(villaDTO);
 
-            return Ok(villaDTO);
+            return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
         }
     }
 }
