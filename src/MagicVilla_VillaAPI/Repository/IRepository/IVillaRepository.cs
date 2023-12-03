3f221034690d2b0adb9409f12deb97037d64b12c
fabiovalinhos <fabiovalinhos@gmail.com>
2023-12-03 18:53:39 -0300

    Criando um repositório genérico

--- src/MagicVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
+++ src/MagicVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
@@ -1,21 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
 using MagicVilla_VillaAPI.Models;
 
 namespace MagicVilla_VillaAPI.Repository.IRepository
 {
-    public interface IVillaRepository
+    public interface IVillaRepository : IRepository<Villa>
     {
-        Task<List<Villa>> GetAllAsync(Expression<Func<Villa, bool>> filter = null);
-        Task<Villa> GetAsync(Expression<Func<Villa, bool>> filter = null, bool tracked = true);
-
-        Task CreateAsync(Villa villa);
-        Task UpdateAsync(Villa villa);
-        Task RemoveAsync(Villa villa);
-
-        Task SaveAsync();
+        Task<Villa> UpdateAsync(Villa villa);
     }
 }
--- src/MagicVilla_VillaAPI/Repository/VillaRepository.cs
+++ src/MagicVilla_VillaAPI/Repository/VillaRepository.cs
@@ -6,64 +6,22 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MagicVilla_VillaAPI.Repository
 {
-    public class VillaRepository : IVillaRepository
+    public class VillaRepository : Repository<Villa>, IVillaRepository
     {
         private readonly ApplicationDbContext _db;
 
-        public VillaRepository(ApplicationDbContext db)
+        public VillaRepository(ApplicationDbContext db) : base(db)
         {
             this._db = db;
         }
 
-        public async Task CreateAsync(Villa villa)
+
+        public async Task<Villa> UpdateAsync(Villa entity)
         {
-            await _db.Villas.AddAsync(villa);
-            await SaveAsync();
-        }
-
-        public async Task<Villa> GetAsync(Expression<Func<Villa, bool>> filter = null, bool tracked = true)
-        {
-            IQueryable<Villa> query = _db.Villas;
-            if (!tracked)
-            {
-                query = query.AsNoTracking();
-            }
-
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-
-            return await query.FirstOrDefaultAsync();
-        }
-
-        public async Task<List<Villa>> GetAllAsync(Expression<Func<Villa, bool>> filter = null)
-        {
-            IQueryable<Villa> query = _db.Villas;
-
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-
-            return await query.ToListAsync();
-        }
-
-        public async Task RemoveAsync(Villa villa)
-        {
-            _db.Villas.Remove(villa);
-            await SaveAsync();
-        }
-
-        public async Task SaveAsync()
-        {
+            _db.Villas.Update(entity);
             await _db.SaveChangesAsync();
-        }
 
-        public async Task UpdateAsync(Villa villa)
-        {
-            _db.Villas.Update(villa);
-            await SaveAsync();
+            return entity;
         }
     }
 }
