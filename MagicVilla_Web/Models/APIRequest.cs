79a7ba985450ceb8d7b694dbdc835844173ea93c
fabiovalinhos <fabiovalinhos@gmail.com>
2023-12-17 15:32:44 -0300

    Criando uma classe library e colocando DTO no projeto web

---
+++ MagicVilla_Web/Models/APIRequest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static MagicVilla_Utility.SD
+
+namespace MagicVilla_Web.Models
+{
+    public class APIRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+
+        public string Url { get; set; }
+
+        public object Data { get; set; }
+    }
+}
