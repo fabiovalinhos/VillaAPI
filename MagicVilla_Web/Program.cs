34f78ce8754a34928f8d332367d39aa3667ddfc1
fabiovalinhos <fabiovalinhos@gmail.com>
2023-12-26 10:44:40 -0300

    Criando o serviço para VillaService e registrando no Program.cs

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -1,11 +1,15 @@
 using AutoMapper;
 using MagicVilla_Web;
+using MagicVilla_Web.Services;
+using MagicVilla_Web.Services.IServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddHttpClient<IVillaService, VillaService>();
+builder.Services.AddScoped<IVillaService, VillaService>();
 
 var configMap = new MapperConfiguration(cfg =>
 {
