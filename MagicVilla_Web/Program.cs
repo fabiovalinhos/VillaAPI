7f712c9e29d8e223371355c64c40ec243ee08895
fabiovalinhos <fabiovalinhos@gmail.com>
2024-01-04 22:30:54 -0300

    Iniciando a lista e obtenção do Villa Number

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -11,6 +11,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient<IVillaService, VillaService>();
 builder.Services.AddScoped<IVillaService, VillaService>();
 
+builder.Services.AddHttpClient<IVillaNumberService, VillaNumberService>();
+builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
+
 var configMap = new MapperConfiguration(cfg =>
 {
     cfg.AddProfile<MappingConfig>();
