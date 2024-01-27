dbb002158be020288064656972e96bc76b2ec66b
fabiovalinhos <fabiovalinhos@gmail.com>
2024-01-27 18:44:53 -0300

    Criando mensagens com SweetAlert

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -45,10 +45,12 @@ namespace MagicVilla_Web.Controllers
                 var response = await _villaService.CreateAsync<APIResponse>(model);
                 if (response != null && response.IsSuccess)
                 {
+                    TempData["success"] = "Villa created successfully";
                     return RedirectToAction(nameof(IndexVilla));
                 }
             }
 
+            TempData["error"] = "Error encountered";
             return View(model);
         }
 
@@ -71,6 +73,7 @@ namespace MagicVilla_Web.Controllers
         {
             if (ModelState.IsValid)
             {
+                TempData["success"] = "Villa updated successfully";
                 var response = await _villaService.UpdateAsync<APIResponse>(model);
                 if (response != null && response.IsSuccess)
                 {
@@ -78,6 +81,7 @@ namespace MagicVilla_Web.Controllers
                 }
             }
 
+            TempData["error"] = "Error encountered";
             return View(model);
         }
 
@@ -101,9 +105,11 @@ namespace MagicVilla_Web.Controllers
             var response = await _villaService.DeleteAsync<APIResponse>(model.Id);
             if (response != null && response.IsSuccess)
             {
+                TempData["success"] = "Villa deleted successfully";
                 return RedirectToAction(nameof(IndexVilla));
             }
 
+            TempData["error"] = "Error encountered";
             return View(model);
         }
     }
