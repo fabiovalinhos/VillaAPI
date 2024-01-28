cd1585b2175747e84aa9e32cd662f9f77f63ebec
fabiovalinhos <fabiovalinhos@gmail.com>
2024-01-28 16:43:49 -0300

    Criando o repositório do User

---
+++ src/MagicVilla_VillaAPI/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO);
+    }
+}
