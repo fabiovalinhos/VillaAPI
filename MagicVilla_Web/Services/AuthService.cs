cb3f91f2393b574f7610566ac669895ffdcaa27e
fabiovalinhos <fabiovalinhos@gmail.com>
2024-02-11 18:27:44 -0300

    Adicionando na web os serviços de login e registro

---
+++ MagicVilla_Web/Services/AuthService.cs
@@ -0,0 +1,39 @@
+using MagicVilla_Utility;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+
+namespace MagicVilla_Web.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string villaUrl;
+
+        public AuthService(IHttpClientFactory httpClient, IConfiguration configuration) : base(httpClient)
+        {
+            this._clientFactory = httpClient;
+            this.villaUrl = configuration.GetValue<string>("ServiceUrls:VillaApi");
+        }
+
+        public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = $"{villaUrl}/api/UserAuth/login"
+            });
+        }
+
+        public Task<T> RegisterAsync<T>(RegistrationRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = $"{villaUrl}/api/UserAuth/register"
+            });
+        }
+    }
+}
