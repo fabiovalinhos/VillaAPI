00b3dea3f8ccc3500e7b637873d31629ff7073a0
fabiovalinhos <fabiovalinhos@gmail.com>
2024-02-14 19:43:42 -0300

    Criado o controller do Auth de login e registro

--- MagicVilla_Web/Controllers/AuthController.cs
+++ MagicVilla_Web/Controllers/AuthController.cs
@@ -1,8 +1,10 @@
+using MagicVilla_Utility;
 using MagicVilla_Web.Models;
 using MagicVilla_Web.Models.Dto;
 using MagicVilla_Web.Services.IServices;
-using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MagicVilla_Web.Controllers
 {
@@ -27,8 +29,20 @@ namespace MagicVilla_Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginRequestDTO obj)
         {
+            APIResponse response = await _authService.LoginAsync<APIResponse>(obj);
+            if (response != null && response.IsSuccess)
+            {
+                LoginResponseDTO model =
+                 JsonConvert.DeserializeObject<LoginResponseDTO>(Convert.ToString(response.Result));
 
-            return View();
+                HttpContext.Session.SetString(SD.SessionToken, model.Token);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", response.ErrorMesages.FirstOrDefault());
+                return View(obj);
+            }
         }
 
         [HttpGet]
@@ -52,8 +66,10 @@ namespace MagicVilla_Web.Controllers
 
         public async Task<IActionResult> Logout()
         {
+            await HttpContext.SignOutAsync();
+            HttpContext.Session.SetString(SD.SessionToken, string.Empty);
 
-            return View();
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult AccessDenied()
--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -16,6 +16,16 @@ builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
 
 builder.Services.AddHttpClient<IAuthService, AuthService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(
+    options =>
+    {
+        options.IdleTimeout = TimeSpan.FromMinutes(100);
+        options.Cookie.HttpOnly = true;
+        options.Cookie.IsEssential = true;
+    }
+);
 
 var configMap = new MapperConfiguration(cfg =>
 {
@@ -39,7 +49,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 app.UseAuthorization();
-
+app.UseSession();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
