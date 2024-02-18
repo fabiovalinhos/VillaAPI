be4a0654d7823ea88f60a2c2b8705c81bc0bdd74
fabiovalinhos <fabiovalinhos@gmail.com>
2024-02-18 14:32:00 -0300

    Iniciando o versionamento da API

--- src/MagicVilla_VillaAPI/Program.cs
+++ src/MagicVilla_VillaAPI/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,6 +33,13 @@ builder.Services.AddSingleton(mapper);
 builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 builder.Services.AddScoped<IVillaNumberRepository, VillaNumberRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+}
+);
 
 var key = builder.Configuration.GetValue<string>("ApiSettings:Secret");
 
